71f0b76e1458fc122267d2d9b929814f82e7629c
m.rau <m.rau@house-of-communication.com>
2025-03-23 11:40:30 +0100

    improve: daemonize the spooler

--- kodosumi/cli.py
+++ kodosumi/cli.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 import click
 
 import kodosumi.service.server
@@ -32,11 +35,13 @@ def cli():
 @click.option('--timeout', default=None, 
               help='Batch retrieval timeout.',
               type=click.FloatRange(min=0, min_open=True))
-@click.option('--force', is_flag=True, default=False, 
-              help='Force spooler to start.')
+@click.option('--start/--stop', is_flag=True, default=True, 
+              help='Run spooler.')
+@click.option('--block', is_flag=True, default=False, 
+              help='Run spooler in foreground (blocking mode).')
 def spooler(ray_server, log_file, log_file_level, level, exec_dir, interval,
-            batch_size, timeout, force):
-    """Start the kodosumi spooler."""
+            batch_size, timeout, start, block):
+    
     kw = {}
     if ray_server: kw["RAY_SERVER"] = ray_server
     if log_file: kw["SPOOLER_LOG_FILE"] = log_file
@@ -47,14 +52,28 @@ def spooler(ray_server, log_file, log_file_level, level, exec_dir, interval,
     if batch_size: kw["SPOOLER_BATCH_SIZE"] = batch_size
     if timeout: kw["SPOOLER_BATCH_TIMEOUT"] = timeout
     settings = Settings(**kw)
-    kodosumi.spooler.main(settings, force=force)
+
+    if start:
+        if block:
+            kodosumi.spooler.main(settings)
+        else:
+            cmd = [sys.executable, "-m", "kodosumi.cli"]
+            cmd += sys.argv[1:] + ["--block"]
+            proc = subprocess.Popen(
+                cmd, 
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                close_fds=True,
+                start_new_session=True
+            )
+            click.echo(f"spooler started.")
+    else:
+        kodosumi.spooler.terminate(settings)
 
 
 @cli.command("serve")
 @click.option("--address", default=None, 
               help="App server URL")
-@click.option("--ray-http", default=None, 
-              help="Ray http server URL")
 @click.option('--log-file', default=None, 
               help='App server log file path.')
 @click.option('--log-file-level', default=None, 
@@ -70,11 +89,14 @@ def spooler(ray_server, log_file, log_file_level, level, exec_dir, interval,
               help='Execution directory.')
 @click.option('--reload', is_flag=True, 
               help='App server reload on file change.')
+@click.option('--start/--stop', is_flag=True, default=True, 
+              help='Run web service.')
+@click.option('--block', is_flag=True, default=False, 
+              help='Run web service in foreground (blocking mode).')
 @click.option("--register", multiple=True, help="Register endpoints")
 
 def server(address, log_file, log_file_level, level, exec_dir, reload,
-           uvicorn_level, ray_http, register):
-    """Start the kodosumi app service."""
+           uvicorn_level, register, start, block):
     kw = {}
     if address: kw["APP_SERVER"] = address
     if log_file: kw["APP_LOG_FILE"] = log_file
@@ -83,8 +105,8 @@ def server(address, log_file, log_file_level, level, exec_dir, reload,
     if exec_dir: kw["EXEC_DIR"] = exec_dir
     if reload: kw["APP_RELOAD"] = reload
     if uvicorn_level: kw["UVICORN_LEVEL"] = uvicorn_level
-    if ray_http: kw["RAY_HTTP"] = ray_http
     if register: kw["REGISTER_FLOW"] = register
+
     settings = Settings(**kw)
     kodosumi.service.server.run(settings)
 
